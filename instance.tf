86a3b17222d815a7a1d9064ea5e071eed52b0d19
root <root@ip-172-31-34-22.ap-south-1.compute.internal>
2025-08-04 15:52:05 +0000

    updated s3 file

--- instance.tf
+++ instance.tf
@@ -1,13 +1,14 @@
 resource "aws_s3_bucket" "my_bucket" {
   bucket = "bbs1"
-
-
 }
 
 resource "aws_s3_bucket_object" "object" {
-  bucket = "bbs1"
+  bucket = aws_s3_bucket.my_bucket.bucket
   key    = "instance.tf"
   source = "${path.module}/instance.tf"
-  etag   = "${filemd5("${path.module}/instance.tf")}"
+  etag   = filemd5("${path.module}/instance.tf")
+
+  depends_on = [aws_s3_bucket.my_bucket]
 }
+
 
